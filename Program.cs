d57ecf0b03afebef1fc92ac1c808e0640997ba50
Fernando Moura <fernandomoura827@gmail.com>
2023-03-18 15:58:58 -0300

    Ignorando arquivos

--- Program.cs
+++ Program.cs
@@ -8,8 +8,8 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
 builder.Services.AddCarter();
+builder.Services.AddSwaggerGen();
 builder.Services.AddDbContext<AppDbContext>(opt => opt.UseInMemoryDatabase("TarefasDb"));
 
 var app = builder.Build();
